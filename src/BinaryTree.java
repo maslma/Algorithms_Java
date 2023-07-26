7245b6464527a7404568276c83724f1646e45a99
maslma <maslmaalzebda23@gmail.com>
2023-07-26 16:19:32 +0300

    Binary tree class commit

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -6,69 +6,63 @@ class BinaryTree {
 
     public BinaryTree() {
         tree = new ArrayList<>();
-        tree.add(null); // Null element at index 0 for simplicity (not used).
+        tree.add(null);
     }
 
-    // Function to insert a new element into the binary tree.
     public void insert(int value) {
         tree.add(value);
     }
 
-    // Function to search for a value in the binary tree.
     public boolean search(int value) {
         return tree.contains(value);
     }
 
-    // Function to perform an inorder traversal of the binary tree.
-    public void inorderTraversal() {
-        inorderTraversal(1); // Start from the root (index 1).
+    public void inorder() {
+        inorder(1);
     }
 
-    private void inorderTraversal(int index) {
+    private void inorder(int index) {
         if (index < tree.size() && tree.get(index) != null) {
-            inorderTraversal(index * 2); // Left child.
+            inorder(index * 2);
             System.out.print(tree.get(index) + " ");
-            inorderTraversal(index * 2 + 1); // Right child.
+            inorder(index * 2 + 1);
         }
     }
 
-    // Function to perform a preorder traversal of the binary tree.
-    public void preorderTraversal() {
-        preorderTraversal(1); // Start from the root (index 1).
+    public void preorder() {
+        preorder(1);
     }
 
-    private void preorderTraversal(int index) {
+    private void preorder(int index) {
         if (index < tree.size() && tree.get(index) != null) {
             System.out.print(tree.get(index) + " ");
-            preorderTraversal(index * 2); // Left child.
-            preorderTraversal(index * 2 + 1); // Right child.
+            preorder(index * 2);
+            preorder(index * 2 + 1);
         }
     }
 
-    // Function to perform a postorder traversal of the binary tree.
-    public void postorderTraversal() {
-        postorderTraversal(1); // Start from the root (index 1).
+    public void postorder() {
+        postorder(1);
     }
 
-    private void postorderTraversal(int index) {
+    private void postorder(int index) {
         if (index < tree.size() && tree.get(index) != null) {
-            postorderTraversal(index * 2); // Left child.
-            postorderTraversal(index * 2 + 1); // Right child.
+            postorder(index * 2);
+            postorder(index * 2 + 1);
             System.out.print(tree.get(index) + " ");
         }
     }
 
-    // Function to get the height of the binary tree.
     public int height() {
-        return height(1); // Start from the root (index 1).
+        return height(1);
     }
 
     private int height(int index) {
         if (index >= tree.size() || tree.get(index) == null) {
             return 0;
         } else {
-            int leftHeight = height(index * 2); // Left child.
-            int rightHeight = height(index * 2 + 1); // Right child.
+            int leftHeight = height(index * 2);
+            int rightHeight = height(index * 2 + 1);
             return 1 + Math.max(leftHeight, rightHeight);
         }
     }
@@ -76,7 +70,6 @@ class BinaryTree {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
 
-        // Insert elements into the binary tree.
         binaryTree.insert(5);
         binaryTree.insert(3);
         binaryTree.insert(7);
@@ -85,26 +78,21 @@ class BinaryTree {
         binaryTree.insert(6);
         binaryTree.insert(8);
 
-        // Inorder traversal.
-        System.out.print("Inorder Traversal: ");
-        binaryTree.inorderTraversal();
+        System.out.print("Inorder :");
+        binaryTree.inorder();
         System.out.println();
 
-        // Preorder traversal.
-        System.out.print("Preorder Traversal: ");
-        binaryTree.preorderTraversal();
+        System.out.print("Preorder : ");
+        binaryTree.preorder();
         System.out.println();
 
-        // Postorder traversal.
-        System.out.print("Postorder Traversal: ");
-        binaryTree.postorderTraversal();
+        System.out.print("Postorder : ");
+        binaryTree.postorder();
         System.out.println();
 
-        // Search for an element.
         int searchValue = 4;
         System.out.println("Element " + searchValue + " found: " + binaryTree.search(searchValue));
 
-        // Get the height of the binary tree.
         System.out.println("Height of the binary tree: " + binaryTree.height());
     }
 }
